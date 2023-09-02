b8f16a0fa39a48a1dfaeda89821db22d050121dc
wuhui <56593113@qq.com>
2023-09-02 23:40:58 +0800

    reflect

--- myreflect/myreflect.go
+++ myreflect/myreflect.go
@@ -5,10 +5,25 @@ import (
 	"reflect"
 )
 
+func add(i int, j int) int {
+
+	return i + j
+}
+
 func UseCase() {
 
 	var i int
 	i = 20
 	rtype := reflect.TypeOf(i)
 	fmt.Println(rtype.Name())
+
+	funval := reflect.ValueOf(add)
+
+	var j int
+	j = 39
+	arr := []reflect.Value{reflect.ValueOf(i), reflect.ValueOf(j)}
+	z := funval.Call(arr)
+
+	fmt.Println("z {}", z[0].Int())
+
 }
